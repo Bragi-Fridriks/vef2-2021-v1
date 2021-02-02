b55c07ff949c05d134ca26855c6fa9586d807c9b
Bragi-Fridriks <brf6@hi.is>
2021-02-02 22:15:37 +0000

    First push

---
+++ app.js
@@ -0,0 +1,39 @@
+const path = require('path');
+const express = require("express");
+
+const videos = require('./src/videos');
+//const content = require('.lib/content');
+
+const app = express();
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+
+//app.locals.setContent = content;
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+function notFoundHandler(req, res, next) {
+  const title = 'Fannst ekki';
+  const message = 'Ó nei, efnið finnst ekki';
+  res.status(404).render('error', { title, message });
+}
+
+function errorHandler(err, req, res, next) {
+  console.error(err);
+  const title = 'Villa kom upp';
+  const message = err.toString();
+  res.status(500).render('error', { title, message });
+}
+
+app.use('/', videos);
+
+app.use(notFoundHandler);
+app.use(errorHandler);
+
+const port = 3000;
+
+app.listen(port, () =>
+  console.log(`App listening at http://localhost:${port}`)
+);
